c01ecacaa0c417bd803a5c48057afa3e6ad2dacf
Firdovsirz <firdovsirz@gmail.com>
2026-02-20 16:18:04 +0400

    feat: product category

--- src/main/java/com/buyology/ecommerce/infrastructure/config/SwaggerUiConfig.java
+++ src/main/java/com/buyology/ecommerce/infrastructure/config/SwaggerUiConfig.java
@@ -6,16 +6,6 @@ import io.swagger.v3.oas.annotations.info.Contact;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(
-    info = @Info(
-        title = "Buyology E-Commerce APIs",
-        version = "1.0",
-        description = "This APIs handle all backend logic and services of the e-commerce applicaition for Buyology. \n Developed and maintained by Firdovsi Rzaev (Dithari Solutions).",
-        contact = @Contact(
-            name = "Firdovsi Rzaev",
-            email = "firdovsirz@gmail.com"
-        )
-    )
-)
+@OpenAPIDefinition(info = @Info(title = "Buyology E-Commerce APIs", version = "1.0", description = "This APIs handle all backend logic and services of the e-commerce applicaition for Buyology. \n Developed and maintained by Firdovsi Rzaev (Dithari Solutions).", contact = @Contact(name = "Firdovsi Rzaev", email = "firdovsirz@gmail.com")))
 public class SwaggerUiConfig {
 }
