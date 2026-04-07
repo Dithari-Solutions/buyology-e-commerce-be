3dd82aaea7bed1019f066e8af77fbde821ea5526
Firdovsirz <firdovsirz@gmail.com>
2026-04-08 01:35:34 +0400

    HMAC validation & courier order support

--- src/main/java/com/buyology/ecommerce/payment/service/PaymobClient.java
+++ src/main/java/com/buyology/ecommerce/payment/service/PaymobClient.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -21,6 +23,8 @@ import org.springframework.web.client.RestTemplate;
  */
 @Component
 public class PaymobClient {
+
+    private static final Logger log = LoggerFactory.getLogger(PaymobClient.class);
 
     private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper;
