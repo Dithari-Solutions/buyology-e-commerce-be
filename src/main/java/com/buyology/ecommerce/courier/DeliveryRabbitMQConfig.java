fb18eea160cf2da30c48586682b526a78ca926ea
Firdovsirz <firdovsirz@gmail.com>
2026-04-20 13:17:47 +0400

    feat: search

--- src/main/java/com/buyology/ecommerce/courier/DeliveryRabbitMQConfig.java
+++ src/main/java/com/buyology/ecommerce/courier/DeliveryRabbitMQConfig.java
@@ -4,7 +4,7 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,13 +25,13 @@ public class DeliveryRabbitMQConfig {
     }
 
     @Bean
-    JacksonJsonMessageConverter jackson2JsonMessageConverter() {
-        return new JacksonJsonMessageConverter();
+    Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 
     @Bean
     RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
-                                  JacksonJsonMessageConverter converter) {
+                                  Jackson2JsonMessageConverter converter) {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
         template.setMessageConverter(converter);
         return template;
